5832acc74515843f745cf30ae58a230645b26bb2
Bogdan Seniuc <me@bogdacutu.me>
2014-12-21 16:16:49 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+KERNEL_PATH=../STLinux-Kernel
+
+cd root
+find . | cpio -o -H newc | gzip > ../initramfs.cpio.gz
+cd ..
+
+cat $KERNEL_PATH/arch/arm/boot/zImage $KERNEL_PATH/arch/arm/boot/dts/tegra124-tn8-p1761-1270-a04-e-battery.dtb > zImage
+
+tools/mkbootimg --kernel zImage --ramdisk initramfs.cpio.gz -o new_boot.img
+
+rm initramfs.cpio.gz
+rm zImage
