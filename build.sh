fc88ede61e2e70adfb2d3f19d22e126bf9f54cd0
Bogdan Seniuc <me@bogdacutu.me>
2015-01-02 00:59:59 -0800

    Use prebuilt DTB

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd root
 find . | cpio -o -H newc | gzip > ../initramfs.cpio.gz
 cd ..
 
-cat $KERNEL_PATH/arch/arm/boot/zImage $KERNEL_PATH/arch/arm/boot/dts/tegra124-tn8-p1761-1270-a04-e-battery.dtb > zImage
+cat $KERNEL_PATH/arch/arm/boot/zImage tegra124-tn8-p1761-1270-a04-e-battery.dtb > zImage
 
 rm new_boot.img
 tools/mkbootimg --kernel zImage --ramdisk initramfs.cpio.gz -o new_boot.img
