d9cc8403a705b33c373f2c7257ee6c172f481f01
Bogdan Seniuc <me@bogdacutu.me>
2015-01-02 00:46:21 -0800

    Mount more partitions

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cd ..
 
 cat $KERNEL_PATH/arch/arm/boot/zImage $KERNEL_PATH/arch/arm/boot/dts/tegra124-tn8-p1761-1270-a04-e-battery.dtb > zImage
 
+rm new_boot.img
 tools/mkbootimg --kernel zImage --ramdisk initramfs.cpio.gz -o new_boot.img
 
 rm initramfs.cpio.gz
