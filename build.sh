aab573c7781ee75471dc0e289eb8d8e584c95a70
Bogdan Seniuc <me@bogdacutu.me>
2015-01-18 03:11:48 -0800

    Added cmdline to build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ cd ..
 
 cat $KERNEL_PATH/arch/arm/boot/zImage tegra124-tn8-p1761-1270-a04-e-battery.dtb > zImage
 
-rm new_boot.img
-tools/mkbootimg --kernel zImage --ramdisk initramfs.cpio.gz -o new_boot.img
+rm -f new_boot.img
+tools/mkbootimg --kernel zImage --ramdisk initramfs.cpio.gz -o new_boot.img --cmdline "console=tty1 fbcon=rotate:2"
 
 rm initramfs.cpio.gz
 rm zImage
